524d0cadf25a774f75c1a73dc88e12ef9279444d
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-05-27 15:23:49 +0300

    Adding start screen outlets

--- Roshambo/ViewController.swift
+++ Roshambo/ViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
+enum GameImages {
+    case rock, paper, scissors
+}
+
 class ViewController: UIViewController {
+
+    // MARK: Outlets
+    
+    @IBOutlet var rock: UIImageView!
+    @IBOutlet var paper: UIImageView!
+    @IBOutlet var scissors: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        rock.image = UIImage(named: "rock")
+        paper.image = UIImage(named: "paper")
+        scissors.image = UIImage(named: "scissors")
     }
 
 }
