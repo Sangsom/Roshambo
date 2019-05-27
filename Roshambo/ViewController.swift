b8766d0f9f80bc3a751598556ed922d9390eee6f
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-05-27 13:48:20 +0300

    Initial Commit

---
+++ Roshambo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Roshambo
+//
+//  Created by Rinalds Domanovs on 27/05/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
